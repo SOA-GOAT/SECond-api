598dd555e06fa11b838734490b22dfb8f1c8b4e8
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2021-10-15 12:45:47 +0800

    Init project_info.rb in step 4.

---
+++ lib/project_info.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'http'
+require 'yaml'
+
+# config = YAML.safe_load(File.read('config/secrets.yml'))
+
+def edgar_api_path(path)
+  "https://data.sec.gov/#{path}"
+end
+
+def call_edgar_url(url) # config, url
+  HTTP.headers('Accept' => '*/*',
+               'Accept-Encoding' => 'gzip, deflate, br',
+               'Connection' => 'keep-alive').get(url)
+end
+
+edgar_response = {}
+edgar_results = {}
+
+
+
+## HAPPY submission request
+submission_url = edgar_api_path("submissions/CIK0000320193.json")
+edgar_response[submission_url] = call_edgar_url(submission_url)
+submission = edgar_response[submission_url].parse
+
+edgar_results['sic'] = submission['sic']
+# should be 3571
+
+edgar_results['sicDescription'] = submission['sicDescription']
+# should be Electronic Computers
+
+edgar_results['name'] = submission['name']
+# should be Apple Inc.
+
+edgar_results['tikers'] = submission['tikers']
+# should be a list ["AAPL"]
+
+### second layer data ###
+# contributors_url = submission['contributors_url']
+# edgar_response[contributors_url] = call_edgar_url()
+# contributors = edgar_response[contributors_url].parse
+
+# edgar_results['contributors'] = contributors
+# contributors.count
+# # should be 3 contributors array
+
+# contributors.map { |c| c['login'] }
+# # should be ["Yuan-Yu", "SOA-KunLin", "luyimin"]
+
+## BAD submission request
+bad_submission_url = edgar_api_path('submissions/CIK0000000000.json')
+edgar_response[bad_submission_url] = call_edgar_url(submission_url)
+edgar_response[bad_submission_url].parse # makes sure any streaming finishes
+
+File.write('spec/fixtures/edgar_results.yml', edgar_results.to_yaml)
