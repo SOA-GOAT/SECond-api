4ab9ea8086fe9cdffb26817a2d380e97ca5c0d42
schwarz-tw <schwarz.tsai@gmail.com>
2021-10-15 15:59:55 +0800

    Add lib/submission.rb, lib/edgar_api.rb in step 5.

---
+++ lib/submission.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SECond
+  # Model for Project
+  class Submission
+    def initialize(submission_data)
+      @submission = submission_data
+    end
+
+    def sic
+      @submission['sic']
+    end
+
+    def sic_description
+      @submission['sicDescription']
+    end
+
+    def name
+      @submission['name']
+    end
+
+    def tickers
+      @submission['tickers']
+    end
+
+  end
+end
