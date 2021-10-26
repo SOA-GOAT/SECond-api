a1d8ea38b6d33f8b74153a3cec49cc6122c7e43f
NianHung <joanne.lee@iss.nthu.edu.tw>
2021-10-26 16:19:39 +0800

    finished step 1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ require 'minitest/rg'
 require 'vcr'
 require 'webmock'
 
-require_relative '../lib/edgar_api'
+require_relative '../init'
 
 CIK = '0000320193'
 CORRECT = YAML.safe_load(File.read('spec/fixtures/edgar_results.yml'))
