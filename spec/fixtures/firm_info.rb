f508f09f1374a912ff5c38d30c798c80ab1b15fc
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2021-10-31 21:30:30 +0800

    Add firm_info.rb back to right folder and Add .reek.yml

---
+++ spec/fixtures/firm_info.rb
@@ -0,0 +1,27 @@
+# # frozen_string_literal: true
+
+# require 'http'
+# require 'yaml'
+# require_relative 'edgar_api'
+
+# edgar_results = {}
+
+# ## HAPPY firm request
+# firm = SECond::EdgarApi.new.firm('0000320193')
+
+# edgar_results['sic'] = firm['sic']
+# # should be 3571
+
+# edgar_results['sicDescription'] = firm['sicDescription']
+# # should be Electronic Computers
+
+# edgar_results['name'] = firm['name']
+# # should be Apple Inc.
+
+# edgar_results['tickers'] = firm['tickers']
+# # should be a list ["AAPL"]
+
+# ## BAD firm request
+# SECond::EdgarApi.new.firm('0000000000')
+
+# File.write('spec/fixtures/edgar_results.yml', edgar_results.to_yaml)
