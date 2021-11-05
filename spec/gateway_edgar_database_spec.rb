57624818a8a8540fb35c605f2e2dcbaa795498da
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2021-11-05 10:10:59 +0800

    Finish step 4 and 6 but not yet debug.

---
+++ spec/gateway_edgar_database_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: false
+
+require_relative 'spec_helper'
+require_relative 'helpers/vcr_helper'
+require_relative 'helpers/database_helper'
+
+describe 'Integration Tests of Edgar API and Database' do
+  VcrHelper.setup_vcr
+
+  before do
+    VcrHelper.configure_vcr_for_github
+  end
+
+  after do
+    VcrHelper.eject_vcr
+  end
+
+  describe 'Retrieve and store firm' do
+    before do
+      DatabaseHelper.wipe_database
+    end
+
+    it 'HAPPY: should be able to save firm from Edgar to database' do
+      firm = SECond::Edgar::FirmMapper.new.find(CIK)
+      rebuilt = SECond::Repository::For.entity(firm).create(firm)
+
+      _(rebuilt.sic).must_equal(firm.sic)
+      _(rebuilt.sic_description).must_equal(firm.sic_description)
+      _(rebuilt.name).must_equal(firm.name)
+      _(rebuilt.tickers).must_equal(firm.tickers)
+
+      # wait to debug
+      firm.submission.each do |submission|
+        found = rebuilt.submission.find do |potential|
+          potential.accession_number == submission.accession_number
+        end
+
+        _(found.firm_name).must_equal member.firm_name
+        # not checking email as it is not always provided
+      end
+    end
+  end
+end
---
+++ spec/helpers/vcr_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'vcr'
+require 'webmock'
+
+# Setting up VCR
+module VcrHelper
+  CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
+  EDGAR_CASSETTE = 'edgar_api'.freeze
+
+  def self.setup_vcr
+    VCR.configure do |c|
+      c.cassette_library_dir = CASSETTES_FOLDER
+      c.hook_into :webmock
+    end
+  end
+
+  def self.configure_vcr_for_github
+
+    VCR.insert_cassette(
+      EDGAR_CASSETTE,
+      record: :new_episodes,
+      match_requests_on: %i[method uri headers]
+    )
+  end
+
+  def self.eject_vcr
+    VCR.eject_cassette
+  end
+end
