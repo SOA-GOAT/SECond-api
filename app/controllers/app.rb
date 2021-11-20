50fc1c3d13ff6b18b6a447ed47fe47df7d09c17a
NianHung <joanne.lee@iss.nthu.edu.tw>
2021-11-20 22:52:07 +0800

    fixing rubocop app.rb

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -15,8 +15,7 @@ module SECond
     plugin :assets, path: 'app/presentation/assets',
                     css: 'style.css' # , js: 'table_row.js'
     use Rack::MethodOverride # for other HTTP verbs (with plugin all_verbs)
-
-    route do |routing| # rubocop:disable Metrics/BlockLength
+    route do |routing|
       routing.assets # load CSS
       routing.public
 
@@ -31,21 +30,22 @@ module SECond
 
         session[:watching] = firms.map(&:get_cik)
 
-        if firms.none?
-          flash.now[:notice] = 'Add a firm cik to get started'
-        end
+        # if firms.none?
+        #   flash.now[:notice] = 'Add a firm cik to get started'
+        # end
+        flash.now[:notice] = 'Add a firm cik to get started' if firms.none?
 
         viewable_firms = Views::FirmsList.new(firms)
-        
-        view 'home', locals: { firms: viewable_firms}
-     end
+        view 'home', locals: { firms: viewable_firms }
+      end
 
       routing.on 'firm' do
         routing.is do
           # POST /firm/
           routing.post do
             firm_cik = routing.params['firm_cik']
-            unless (firm_cik.size <= 10)
+            # unless (firm_cik.size <= 10)
+            if firm_cik.size > 10
               flash[:error] = 'CIK exceeds 10-digit length'
               routing.redirect '/'
             end
@@ -54,13 +54,12 @@ module SECond
             firm_cik = format('%010d', firm_cik.to_i)
 
             begin
-              edgar_firm = Repository::For.klass(Entity::Firm).find_cik(firm_cik) 
+              edgar_firm = Repository::For.klass(Entity::Firm).find_cik(firm_cik)
 
               if edgar_firm.nil? == false
                 session[:watching].insert(0, firm_cik).uniq!
                 routing.redirect "firm/#{firm_cik}"
               end
-
             rescue StandardError => error
               flash[:error] = 'Having trouble accessing the database'
               routing.redirect '/'
@@ -108,15 +107,15 @@ module SECond
               flash[:error] = 'Having trouble accessing the database'
               routing.redirect '/'
             end
-              # Download 10-Ks from project information
-              # api = Edgar::EdgarApi.new
-              # firm_filings = edgar_firm.filings.select { |filing| filing.form_type.include? "10-K"}
-              # firm_filings.each do |filing|
-              # api.download_submission_url(firm_cik, filing.accession_number)
+            # Download 10-Ks from project information
+            # api = Edgar::EdgarApi.new
+            # firm_filings = edgar_firm.filings.select { |filing| filing.form_type.include? "10-K"}
+            # firm_filings.each do |filing|
+            # api.download_submission_url(firm_cik, filing.accession_number)
             begin
               firm_filings = FirmFiling.new(edgar_firm)
               firm_filings.download! unless firm_filings.exists_locally?
-              # end
+            # end
             rescue StandardError => error
               flash[:error] = 'Could not download filings'
               routing.redirect '/'
@@ -131,7 +130,8 @@ module SECond
               routing.redirect '/'
             end
             firm = Views::Firm.new(edgar_firm)
-            firm_rdbr = Views::FirmReadability.new(firm_rdb)
+            # firm_rdbr = Views::FirmReadability.new(firm_rdb)
+            firm_rdb = Views::FirmReadability.new(firm_rdb)
             # Show viewer the firm
             view 'firm', locals: { firm: firm, firm_rdb: firm_rdb }
           end
